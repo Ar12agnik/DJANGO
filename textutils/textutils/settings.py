114d67e651e3df8593b05e999195d3cc37150995
Agnik Roy <Ar12agnik@gmail.com>
2023-08-11 23:29:25 +0530

    fgh

--- textutils/textutils/settings.py
+++ textutils/textutils/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-!w1k_5ln@1=)m$5+zp#rq518fi1*8t1rd64aikp!e5#z265z=1"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['textoptionsar.azurewebsites.net']
 
